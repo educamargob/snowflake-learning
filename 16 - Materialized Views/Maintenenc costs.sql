69fb00713dab76ed0a17a110caee167cd1e92190
educamargob <ecamargoborges@gmail.com>
2025-09-27 19:40:04 +0000

    Materialized Views

---
+++ 16 - Materialized Views/Maintenenc costs.sql
@@ -0,0 +1,9 @@
+
+SHOW MATERIALIZED VIEWS;
+
+
+
+select * from table(information_schema.materialized_view_refresh_history()); 
+    DATE_RANGE_START => '2025-09-26 12:20:28.085 -0700' ));
+
+SELECT CURRENT_TIMESTAMP;
